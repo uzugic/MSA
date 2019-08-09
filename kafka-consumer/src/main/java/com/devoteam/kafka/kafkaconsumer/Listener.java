d4418664ddcd29fa09ecea6fc95d884041517314
mile <mile.miljanovic@devoteam.com>
2019-08-09 09:16:50 +0200

    Minor correction in listener

--- kafka-consumer/src/main/java/com/devoteam/kafka/kafkaconsumer/Listener.java
+++ kafka-consumer/src/main/java/com/devoteam/kafka/kafkaconsumer/Listener.java
@@ -54,19 +54,21 @@ public class Listener {
 		//System.out.println("Message has arrived: Content: " + message);
 		String[] spt = message.split("Id is: ");
 		String id = spt[1];
-		HashMap<String, String> hmap = new HashMap<String, String>();
-		hmap.put("id", id);
-		hmap.put("username", "User" + id);
-		hmap.put("password", "Password" + id);
-		hmap.put("age", "27");
-		hmap.put("gender", "TRIGGERED");
-		hmap.put("attribute1", "attribute1Val");
-		hmap.put("attribute2", "attribute2Val");
-		hmap.put("attribute3", "attribute3Val");
-		hmap.put("attribute4", "attribute4Val");
-		hmap.put("attribute5", "attribute5Val");
-		hmap.put("attribute6", "attribute6Val");
-		jedis.hmset("user:" + id, hmap);
+		if (id != null) {
+			HashMap<String, String> hmap = new HashMap<String, String>();
+			hmap.put("id", id);
+			hmap.put("username", "User" + id);
+			hmap.put("password", "Password" + id);
+			hmap.put("age", "27");
+			hmap.put("gender", "TRIGGERED");
+			hmap.put("attribute1", "attribute1Val");
+			hmap.put("attribute2", "attribute2Val");
+			hmap.put("attribute3", "attribute3Val");
+			hmap.put("attribute4", "attribute4Val");
+			hmap.put("attribute5", "attribute5Val");
+			hmap.put("attribute6", "attribute6Val");
+			jedis.hmset("user:" + id, hmap);
+		}
 		
 	}
 	
